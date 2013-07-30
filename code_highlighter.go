d3a06801d7b165e6cb961255186fbdc79853d995
Andrew Vos <andrew.vos@gmail.com>
2013-07-30 19:22:53 +0100

    update example to contain pygments directly

--- code_highlighter.go
+++ code_highlighter.go
@@ -3,8 +3,11 @@ package gaki
 import (
   "strings"
   "regexp"
-  "github.com/AndrewVos/pygmentizer"
   "fmt"
+  "io"
+  "errors"
+  "path"
+  "os/exec"
 )
 
 func HighlightCode(html string) string {
@@ -12,7 +15,7 @@ func HighlightCode(html string) string {
   for _,matches:= range matcher.FindAllStringSubmatch(html, -1) {
     lang := matches[1]
     code := matches[2]
-    highlighted, err := pygmentizer.Highlight(lang, code)
+    highlighted, err := highlightWithPygments(lang, code)
     if err == nil {
       html = strings.Replace(html, matches[0], highlighted, 1)
     } else {
@@ -20,4 +23,23 @@ func HighlightCode(html string) string {
     }
   }
   return html
+}
+
+func pygmentizerPath() string {
+  return path.Join("vendor", "pygments", "pygmentizer.py")
+}
+
+func highlightWithPygments(language string, code string) (string, error) {
+  cmd:= exec.Command("/usr/bin/env", "python", pygmentizerPath(), "-l", language, "-f", "html")
+  writer, _ := cmd.StdinPipe()
+
+  io.WriteString(writer, code)
+  writer.Close()
+  output,err := cmd.CombinedOutput()
+
+  if err != nil {
+    fmt.Printf(string(output))
+    return code, errors.New(string(output))
+  }
+  return string(output), nil
 }
