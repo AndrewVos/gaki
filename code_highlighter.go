528d5c6d8437a2f9705c050f4940511219b97be1
Andrew Vos <andrew.vos@gmail.com>
2013-07-30 14:05:22 +0100

    use pygmentizer again

--- code_highlighter.go
+++ code_highlighter.go
@@ -10,11 +10,13 @@ import (
 func HighlightCode(html string) string {
   matcher := regexp.MustCompile("<pre>:::(\\w+)((?s).*?)</pre>")
   for _,matches:= range matcher.FindAllStringSubmatch(html, -1) {
-    highlighted, err := pygmentizer.Highlight(matches[1], matches[2])
+    lang := matches[1]
+    code := matches[2]
+    highlighted, err := pygmentizer.Highlight(lang, code)
     if err == nil {
-      fmt.Print(err)
-    } else {
       html = strings.Replace(html, matches[0], highlighted, 1)
+    } else {
+      fmt.Print(err)
     }
   }
   return html
